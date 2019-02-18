34a88e4770c0cbb037ac1520967e369647a1941a
JustinJayOh <jjoh1993@gmail.com>
2019-02-17 21:46:20 -0800

    Update Grid.cs
    Added class documentation and method docs.

--- TicTacToe/MainForm/Grid.cs
+++ TicTacToe/MainForm/Grid.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/** 
+* @author Justin Oh
+* @email jjoh1993@gmail.com
+* @version 2/17/2019
+*
+* Grid class to hold all pieces remaining on grid.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +21,12 @@ namespace MainForm
         static List<int> piece = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 
         // Methods
+        
+        /**
+        * Removes piece from grid.
+        * 
+        * @param int x for magic square number corresponding to grid space.
+        */
         public static void RemovePiece(int x)
         {
             if (ContainsPiece(x))
@@ -20,7 +34,13 @@ namespace MainForm
                 piece.Remove(x);
             }
         }
-
+        
+        /**
+        * Returns true if grid contains piece.
+        *
+        * @param int x for magic sqaure number corresponding to grid space.
+        * @return true if grid contains piece.
+        */
         public static Boolean ContainsPiece(int x)
         {
             if (piece.Contains(x))
@@ -29,7 +49,11 @@ namespace MainForm
             }
             return false;
         }
-
+        
+        /**
+        * Resets all pieces on grid.
+        *
+        */
         public static void ResetPiece()
         {
             piece.Clear();
